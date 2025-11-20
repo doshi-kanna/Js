2e7df8b4693a8423e8bc9f9c303beffa4dce8274
doshi-kanna <doshi-kanna>
2025-11-20 16:09:44 +0530

    feat: file added variable.html

---
+++ Js_topics/Day1/script.js
@@ -0,0 +1,24 @@
+console.log("Hello, welcome to Day 1 of JavaScript learning!");
+alert("Please enter your details to proceed.");
+//variable declaration
+let bookname;
+//variable initialization
+bookname = "The lion King";
+console.log("Book Name:", bookname);
+let bookAuthor="John Smith";
+console.log("Book Author:", bookAuthor);
+let bookPrice=250;
+console.log("Book Price:", bookPrice);
+let isAvailable=true;
+console.log("Is Book Available?:", isAvailable);
+let bookEdition;
+console.log("Book Edition:", bookEdition);
+bookEdition="First Edition";
+console.log("Book Edition after initialization:", bookEdition);
+
+
+
+let a,b,c;
+a=5,b=10,c=14;
+console.log("addition of three: ",a+b+c);
+console.log(a,"\n",b,"\n",c);
